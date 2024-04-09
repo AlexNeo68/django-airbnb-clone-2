d84985713921934cae46675d94be6cf4d8f7adde
Александр Неудахин <aleksandrneudahin@Aleksandrs-Air.localnet>
2024-04-09 07:55:00 +0300

    pillow

--- users/models.py
+++ users/models.py
@@ -1,7 +1,22 @@
+from random import choices
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 # Create your models here.
 class User(AbstractUser):
-    bio = models.TextField(default='')
+    """Custom User Model"""
+
+    MALE = 'MALE'
+    FEMALE = 'FEMALE'
+    OTHER = 'OTHER'
+
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHER, 'Other'),
+    )
+
+    bio = models.TextField(default='', blank=True)
+    gender = models.CharField(max_length=10, null=True, blank=True, choices=GENDER_CHOICES)
+    avatar = models.ImageField(null=True, blank=True)

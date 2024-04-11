9eb68d37755226cf456f5c9488463780d1f4678c
Александр Неудахин <aleksandrneudahin@Aleksandrs-MacBook-Air.local>
2024-04-11 07:47:48 +0300

    rooms

--- config/settings.py
+++ config/settings.py
@@ -37,11 +37,18 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-PROJECT_APPS = [
-    "users.apps.UsersConfig"
+
+THIRD_PARTY_APPS = [
+    "django_countries"
 ]
 
-INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
+PROJECT_APPS = [
+    "core.apps.CoreConfig",
+    "rooms.apps.RoomsConfig",
+    "users.apps.UsersConfig",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampedModel
+from users.models import User
+from django_countries.fields import CountryField
+
+
+class Room(TimeStampedModel):
+    
+    """Model for Room entity"""
+
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=140)
+    address = models.CharField(max_length=140)
+    guests = models.IntegerField()
+    beds = models.IntegerField()
+    bathrooms = models.IntegerField()
+    price = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False) #занято ли
+    host = models.ForeignKey(User, on_delete=models.CASCADE) #владелец

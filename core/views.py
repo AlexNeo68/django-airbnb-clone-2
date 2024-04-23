817ceea2d20f44d9558606318d678f3b563d4daf
Alexander <alexander@iMac-Alexander.local>
2024-04-23 08:49:27 +0300

    73

--- core/views.py
+++ core/views.py
@@ -1,14 +1,25 @@
-from django.http import HttpResponse
+from math import ceil
+
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from rooms.models import Room
 
 
 def all_rooms(request):
+    page = request.GET.get('page', 1)
+    page = int(page or 1)
+    limit = request.GET.get('limit', 10)
+    limit = int(limit or 10)
+
     rooms = Room.objects.all()
-    hungry = False
+    paginator = Paginator(rooms, limit, orphans=5)
+
+    rooms_list = paginator.get_page(page)
+
+    print(vars(rooms_list))
+
     context = {
-        'rooms': rooms,
-        'hungry': hungry
+        'page': rooms_list,
     }
     return render(request, 'rooms/index.html', context)
